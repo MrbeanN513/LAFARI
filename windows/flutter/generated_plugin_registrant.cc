02ea0cfb83d9908107b3bff612943910e659ecb7
Mrbeann513 <haldergoutam9@gmail.com>
2021-10-15 02:39:34 -0700

    sc

---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <bitsdojo_window_windows/bitsdojo_window_plugin.h>
+#include <flutter_acrylic/flutter_acrylic_plugin.h>
+#include <webview_windows/webview_windows_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  BitsdojoWindowPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("BitsdojoWindowPlugin"));
+  FlutterAcrylicPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterAcrylicPlugin"));
+  WebviewWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WebviewWindowsPlugin"));
+}
